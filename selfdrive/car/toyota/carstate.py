0087c5d7b07e33d4a2ea7c81eb0630772b0fc6ae
aivordin <285717086@qq.com>
2026-07-05 13:09:27 +0800

    Update carstate.py

--- selfdrive/car/toyota/carstate.py
+++ selfdrive/car/toyota/carstate.py
@@ -208,7 +208,7 @@ class CarState(CarStateBase):
       # distance button is wired to the ACC module (camera or radar)
       self.prev_distance_button = self.distance_button
       if self.CP.carFingerprint in (SECOC_CAR - RADAR_ACC_CAR):
-        self.distance_button = cp.vl["PCM_CRUISE_4"]["DISTANCE"]
+        self.distance_button = cp_acc.vl["ACC_CONTROL"]["DISTANCE"]
       elif self.CP.carFingerprint in (TSS2_CAR - RADAR_ACC_CAR):
         self.distance_button = cp_acc.vl["ACC_CONTROL"]["DISTANCE"]
       else:
@@ -278,7 +278,7 @@ class CarState(CarStateBase):
         ("GAS_PEDAL", 42),
       ]
       if CP.carFingerprint not in RADAR_ACC_CAR:
-        messages.append(("PCM_CRUISE_4", 1))
+        # messages.append(("PCM_CRUISE_4", 1))
     else:
       messages.append(("VSC1S07", 20))
       if CP.carFingerprint not in [CAR.TOYOTA_MIRAI]:
